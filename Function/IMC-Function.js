ceddd84dbdea1ba1d390650b0426d867ea000dc1
juLia <84445011+ttpmorp@users.noreply.github.com>
2023-08-25 08:17:29 -0300

    Update IMC-Function.js

--- Function/IMC-Function.js
+++ Function/IMC-Function.js
@@ -2,15 +2,15 @@ function calcularIMC(peso, altura) {
     const imc = peso / (altura * altura);
 
     if (imc < 18.5) {
-        console.log('Logo, você está ABAIXO DO PESO!');
+        console.log('você está ABAIXO DO PESO!');
     } else if (imc >= 18.5 && imc <= 25) {
-        console.log('Logo, você está com PESO NORMAL!');
+        console.log('você está com PESO NORMAL!');
     } else if (imc >= 25 && imc <= 30) {
-        console.log('Logo, você está ACIMA DO PESO!');
+        console.log('você está ACIMA DO PESO!');
     } else if (imc >= 30 && imc <= 40) {
-        console.log('Logo, você está OBESO!');
+        console.log('você está OBESO!');
     } else {
-        console.log('Logo, você está com OBESIDADE GRAVE!')
+        console.log('você está com OBESIDADE GRAVE!')
     }
 }
 
