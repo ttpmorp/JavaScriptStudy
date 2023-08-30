0a47bd0444bb13968eef522ff979e3657319ae60
juLia <84445011+ttpmorp@users.noreply.github.com>
2023-08-30 15:34:10 -0300

    Update randow.js

--- Importation/randow.js
+++ Importation/randow.js
@@ -1,16 +1,16 @@
-const entradas = [5, 50, 10, 98, 23];
-let i = 0;
+const entradas = [5, 50, 10, 98, 23]; // Valores de entrada fictícios
 
-function gets(){
-    const valor = entradas[i];
-    i++;
-    return valor;
+let i = 0; // Índice para controlar qual valor de entrada será usado
 
+function gets() {
+    const valor = entradas[i]; // Obtém o valor atual do array 'entradas'
+    i++; // Incrementa o índice para apontar para o próximo valor de entrada
+    return valor; // Retorna o valor obtido
 }
 
-function print(texto){
-    console.log(texto);
+function print(texto) {
+    console.log(texto); // Imprime o texto no console
 }
 
-
-module.exports = { gets, print};
+// Exporta as funções 'gets' e 'print' para uso em outros módulos
+module.exports = { gets, print };
