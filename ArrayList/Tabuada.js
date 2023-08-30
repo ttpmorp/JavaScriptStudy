10ae6106a97a826a0c85962fd66ad938b2ab2baf
juLia <84445011+ttpmorp@users.noreply.github.com>
2023-08-30 15:30:26 -0300

    Update Tabuada.js

--- ArrayList/Tabuada.js
+++ ArrayList/Tabuada.js
@@ -1,5 +1,6 @@
-const n = 6;
+const n = 6; // Número para o qual será calculada a tabuada
 
+// Loop que multiplica 'n' pelos números de 1 a 10
 for (let i = 1; i <= 10; i++) {
-    console.log(i * n);
+    console.log(i * n); // Imprime o resultado da multiplicação
 }
