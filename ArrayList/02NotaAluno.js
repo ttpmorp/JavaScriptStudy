e7e19357725c05bd4a87de1128b588aec64bfc98
juLia <84445011+ttpmorp@users.noreply.github.com>
2023-08-30 15:27:45 -0300

    Update 02NotaAluno.js

--- ArrayList/02NotaAluno.js
+++ ArrayList/02NotaAluno.js
@@ -1,11 +1,14 @@
 const notas = [];
 
+// Adicionar valores ao array de notas
 notas.push(5);
 notas.push(7);
 notas.push(8);
 notas.push(2);
 notas.push(5);
 
+// Calcular a soma das notas
 const soma = notas[0] + notas[1] + notas[2] + notas[3] + notas[4];
 
+// Calcular e imprimir a média das notas
 console.log(soma / 5);
