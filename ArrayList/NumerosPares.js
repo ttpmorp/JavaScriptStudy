5cd1e8822d869cf14a4c7b0ff1b4903307acf56d
juLia <84445011+ttpmorp@users.noreply.github.com>
2023-08-30 15:29:47 -0300

    Update NumerosPares.js

--- ArrayList/NumerosPares.js
+++ ArrayList/NumerosPares.js
@@ -1,8 +1,7 @@
 const n = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// Loop que percorre os índices pares do array 'n'
 for (let i = 0; i < n.length; i = i + 2) {
-    const par = n[i];
-    if (par % 2 === 0){
-        console.log(par);
-    }
-    
+    // Obtém o número par atual no índice 'i' do array 'n'
+    console.log(n[i]);
 }
