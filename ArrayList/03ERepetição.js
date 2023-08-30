431f053da43cef6ff58ae8da6bfd31820e63979f
juLia <84445011+ttpmorp@users.noreply.github.com>
2023-08-30 15:28:18 -0300

    Update 03ERepetição.js

--- ArrayList/03ERepetição.js
+++ ArrayList/03ERepetição.js
@@ -1,6 +1,11 @@
 const nome = 'Julia C Silva';
 
+// Loop que percorre cada caractere da string 'nome'
 for (let i = 0; i < nome.length; i++) {
+    // Obtém o caractere na posição 'i' da string 'nome'
     const letra = nome[i];
+    
+    // Imprime a letra atual no console
     console.log(letra);
 }
+
