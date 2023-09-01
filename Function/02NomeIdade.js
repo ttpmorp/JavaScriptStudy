436468e8d7ee4c74d9b06cbad934522518d9f4be
juLia <84445011+ttpmorp@users.noreply.github.com>
2023-08-31 23:27:59 -0300

    Update 02NomeIdade.js

--- Function/02NomeIdade.js
+++ Function/02NomeIdade.js
@@ -1,17 +1,22 @@
-function escrevaNome(nome){
+// Definição da função escrevaNome
+function escrevaNome(nome) {
     console.log('Meu nome é: ' + nome);
 }
 
+// Chamada da função escrevaNome com o argumento 'Júlia'
 escrevaNome('Júlia');
+
+// Chamada da função escrevaNome com o argumento 'Bob'
 escrevaNome('Bob');
 
-function verificarIdade(idade){
-    if(idade <= 18){
-        console.log('Menor de idade!')
-    } 
-    else{
-        console.log('Maior de idade!')
+// Definição da função verificarIdade
+function verificarIdade(idade) {
+    if (idade <= 18) {
+        console.log('Menor de idade!');
+    } else {
+        console.log('Maior de idade!');
     }
 }
 
+// Chamada da função verificarIdade com o argumento 23
 verificarIdade(23);
