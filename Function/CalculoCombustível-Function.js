a80d3ffac6a44854232b03ea61d8839498972686
juLia <84445011+ttpmorp@users.noreply.github.com>
2023-08-31 23:31:33 -0300

    Update CalculoCombustível-Function.js

--- Function/CalculoCombustível-Function.js
+++ Function/CalculoCombustível-Function.js
@@ -1,12 +1,16 @@
+// Definição da função calcularValorViagem
 function calcularValorViagem(precoCombustivel, gastoCombustivel, distanciaViagem) {
     const valorViagem = (distanciaViagem / gastoCombustivel) * precoCombustivel;
-    return valorViagem.toFixed(2);
+    return valorViagem.toFixed(2); // Retorna o valor da viagem formatado com duas casas decimais
 }
 
+// Valores utilizados para os cálculos
 const precoCombustivel = 5.79;
 const gastoCombustivel = 10;
 const distanciaViagem = 100;
 
+// Chamada da função calcularValorViagem com os argumentos dados
 const valorViagemCalculado = calcularValorViagem(precoCombustivel, gastoCombustivel, distanciaViagem);
 
+// Imprime o valor calculado da viagem
 console.log(valorViagemCalculado);
