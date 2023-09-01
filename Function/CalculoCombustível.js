b18368fc7ee00c04f475c4e43e5f2cad66ef1d69
juLia <84445011+ttpmorp@users.noreply.github.com>
2023-08-31 23:32:54 -0300

    Update CalculoCombustível.js

--- Function/CalculoCombustível.js
+++ Function/CalculoCombustível.js
@@ -1,7 +1,10 @@
+// Definição do preço do combustível, consumo de combustível e distância da viagem
 const preco_combustivel = 5.79;
 const gasto_combustivel = 10;
 const distancia_viagem = 100;
 
+// Cálculo do valor da viagem
 const valor_viagem = (distancia_viagem / gasto_combustivel) * preco_combustivel;
 
- console.log(valor_viagem.toFixed(2));
+// Imprime o valor da viagem formatado com duas casas decimais
+console.log(valor_viagem.toFixed(2));
