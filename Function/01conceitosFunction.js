d28569cc31715a3ea096f08f2c13070599dda068
juLia <84445011+ttpmorp@users.noreply.github.com>
2023-08-31 23:27:17 -0300

    Update 01conceitosFunction.js

--- Function/01conceitosFunction.js
+++ Function/01conceitosFunction.js
@@ -1,9 +1,10 @@
-// Conceitos Básicos Funções
-
-function sayMyName(name){
-    console.log('your name is: ', name);
-
+// Definição de uma função chamada sayMyName
+function sayMyName(name) {
+    console.log('your name is:', name);
 }
 
+// Chamada da função sayMyName com o argumento 'Julia'
 sayMyName('Julia');
+
+// Chamada da função sayMyName com o argumento 'Cristinny'
 sayMyName('Cristinny');
